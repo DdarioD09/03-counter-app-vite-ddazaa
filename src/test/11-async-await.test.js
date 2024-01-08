b2c81e3cda2390c5ac9b0b9e192903876d44937d
ddariod <dariod.dazaa09@gmail.com>
2024-01-08 16:57:54 +1100

    create async tests

---
+++ src/test/11-async-await.test.js
@@ -0,0 +1,18 @@
+import { getImagen } from '../base-pruebas/11-async-await'
+
+describe('Tests on 11-async-await.js', () => {
+
+    test('getImagen should return an URL image', async () => {
+        const url = await getImagen()
+            .then((url) => {
+                expect(typeof url).toBe('string')
+            });
+    })
+
+    test('getImagen should return an error message', async () => {
+        const url = await getImagen()
+            .catch((error) => {
+                expect(error).toBe('Image not found')
+            });
+    })
+})
